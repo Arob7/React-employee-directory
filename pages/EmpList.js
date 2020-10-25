948aea547edace49fdd81a2efdbf078b3ab4e55f
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-10-25 19:20:01 -0400

    added pages folder

---
+++ pages/EmpList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+
+// LOADS USER INFO
+// loadUser = () => {
+//   API.getusers()
+//     .then((res) =>
+//       this.setState({
+//         image: res.data.message,
+//       })
+//     )
+//     .catch((err) => console.log(err));
+// };
